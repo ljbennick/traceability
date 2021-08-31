aca8b229e57128007b51face03e709888f872047
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-31 14:02:50 -0600

    added rollbar and express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+    accessToken: '',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+rollbar.log("Hello world")
+
+const app = express()
+app.use(express.json())
