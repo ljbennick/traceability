2df27fef05c36710341ce6ddbcdd3ee41e63017d
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-31 14:06:23 -0600

    comment out

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
 
-const rollbar = new Rollbar({
-    accessToken: '',
-    captureUncaught: true,
-    captureUnhandledRejections: true
-})
+// const rollbar = new Rollbar({
+//     accessToken: '',
+//     captureUncaught: true,
+//     captureUnhandledRejections: true
+// })
 rollbar.log("Hello world")
 
 const app = express()
