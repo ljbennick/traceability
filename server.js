677c6b4f968f3e976f6d129367a37c3e3d1e1282
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-31 14:11:29 -0600

    added access key

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
 
-// const rollbar = new Rollbar({
-//     accessToken: '',
-//     captureUncaught: true,
-//     captureUnhandledRejections: true
-// })
+const rollbar = new Rollbar({
+    accessToken: 'b8e3a32e1f684be495c8e577606c9582',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 rollbar.log("Hello world")
 
 const app = express()
